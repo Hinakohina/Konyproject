5d5c35f77d06b2c8d415139fa06b95381aae0763
Hinakohina <jeremyacevendo@gmail.com>
2025-09-18 16:38:17 -0600

    odio lenguaje C

---
+++ konycomepatata.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main (void){
+printf ("habia una vez que kony empezo a comer patata y se puso a comer mucho patata\n");
+printf("como kony era traviesa empezo a comer patatas de su madre y como su madre no perdona,le dio un castigo de un dia sin ver brainrot\n");
+}
